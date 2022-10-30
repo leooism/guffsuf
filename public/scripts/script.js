783305565d13e60fe0cd13a8a6e5da6c0189d52d
Bidhan <leooism10@gmail.com>
2022-10-30 18:58:47 +0545

    establish peer to peer connection

---
+++ public/scripts/script.js
@@ -0,0 +1,183 @@
+const inputBox = document.querySelector("#input-box");
+const sendBtn = document.querySelector(".send-btn");
+const videoGrid = document.querySelector(".video-grid");
+const participantGrid = document.querySelector(".video-participant");
+const videoHost = document.querySelector(".video-host");
+
+const chatMessage = document.querySelector(".chat-message");
+
+const socket = io("/");
+const peer = new Peer(undefined, {
+	port: "3001",
+	host: "/",
+});
+const pinVideo = true;
+
+const constraints = {
+	video: true,
+	audio: false,
+};
+
+const onSucess = async (videoEle, stream, type = undefined) => {
+	videoEle.srcObject = stream;
+	videoEle.addEventListener("loadedmetadata", (e) => {
+		videoEle.play();
+	});
+	if (type === "participant") return participantGrid.appendChild(videoEle);
+	if (type === "host") return videoHost.appendChild(videoEle);
+	videoGrid.appendChild(videoEle);
+};
+
+const onError = (error) => {
+	console.log(error);
+};
+
+const getVideoTrack = async function (options) {
+	return await navigator.mediaDevices.getUserMedia(options);
+};
+
+peer.on("open", (clientid) => {
+	socket.emit("join-room", id, clientid);
+});
+const connectToNewUser = (stream, clientid) => {
+	const call = peer.call(clientid, stream);
+
+	call.on("stream", (remoteStream) => {
+		console.log(remoteStream);
+		const video = document.createElement("video");
+		video.classList.add("participant");
+		onSucess(video, remoteStream);
+		// rootVideo.srcObject = remoteStream;
+	});
+	call.on("close", () => {
+		console.log("call is closed");
+	});
+};
+getVideoTrack(constraints).then((stream) => {
+	console.log(stream.getVideoTracks());
+	const video = document.createElement("video");
+	onSucess(video, stream);
+
+	peer.on("call", (call) => {
+		call.answer(stream);
+		call.on("stream", (stream) => {
+			console.log(stream);
+			const video = document.createElement("video");
+			// video.classList.add("host");
+			onSucess(video, stream);
+		});
+	});
+	socket.on("user-connected", (id) => {
+		connectToNewUser(stream, id);
+	});
+});
+sendBtn.addEventListener("click", (e) => {
+	e.preventDefault();
+	const msg = inputBox.value;
+	const msgBox = document.createElement("p");
+	msgBox.classList.add("sender");
+	const p = document.createElement("span");
+	p.classList.add("sender-msg");
+	p.innerText = msg;
+	msgBox.appendChild(p);
+
+	chatMessage.appendChild(msgBox);
+
+	socket.emit("message", msg);
+	inputBox.value = "";
+});
+socket.on("createMessage", (msg) => {
+	const msgBox = document.createElement("p");
+	msgBox.classList.add("reciever");
+	const p = document.createElement("soab");
+	p.classList.add("reciever-msg");
+	p.innerText = msg;
+	msgBox.appendChild(p);
+	chatMessage.appendChild(msgBox);
+});
+
+// const getListOfInputDevices = async () => {
+// 	const devices = await navigator.mediaDevices.enumerateDevices();
+// 	return devices;
+// };
+// getListOfInputDevices().then((devices) => console.log(devices));
+
+// We should constantly look for media device changes
+
+/* 
+*Create a RTCPeerConnection for each end of the call and 
+*at each end, add the local stream from getUserMedia()
+
+
+* Get and share network information: potential connection 
+* endpoints are known as ICE candidates
+
+* Get and share local and remote descriptions: metadata
+* about local media in SDP format.
+
+
+
+*/
+
+// const server = {
+// 	iceServers: [
+// 		{
+// 			url: "stun:stun.xten.com",
+// 		},
+// 		{
+// 			// Use my TURN server on DigitalOcean
+// 			url: "turn:numb.viagenie.ca",
+// 			credential: "sunghiep",
+// 			username: "nghiepnds@yahoo.com",
+// 		},
+// 	],
+// };
+// let stream;
+
+/* 
+! We should specify STUN and TURN servers in the server argument.
+
+
+*/
+
+// const pc1 = new RTCPeerConnection(server);
+// const pc2 = new RTCPeerConnection(server);
+// let answer, offer;
+
+//Exchange Network information -> finding network interfaces and ports using ICE framework
+
+// pc1.onicecandidate = (e) => gotIceCandidate(pc1, e);
+// pc2.onicecandidate = (e) => gotIceCandidate(pc2, e);
+
+// pc2.ontrack = gotRemoteStream;
+// function gotIceCandidate(peer, event) {
+// 	console.log(peer);
+// 	if (!event.candidate) return;
+// 	getOtherPc(peer).addIceCandidate(event.candidate);
+// }
+
+// const createConnection = async () => {};
+
+// function getOtherPc(pc) {
+// 	return pc === pc1 ? pc2 : pc1;
+// }
+
+// function gotRemoteStream(e) {
+// 	if (remoteElement.srcObject === e) {
+// 		return;
+// 	}
+// 	remoteElement.srcObject = e;
+// }
+
+// const makeCall = async (connection) => {
+// 	onSucess(getVideoTrack(constraints));
+
+// 	getVideoTrack(constraints).then(async (stream) => {
+// 		// connection.addTrack(track, stream);
+// 		if (stream) socket.emit("offer", stream);
+// 	});
+// };
+
+/* 
+
+*/
---
+++ server.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+const { v4: uuidv4 } = require("uuid");
+
+const cors = require("cors");
+const path = require("path");
+const { createSocket } = require("dgram");
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", "./public");
+app.use(express.static(path.join(__dirname, "public")));
+
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+	cors: {
+		origin: "https://localhost:5500",
+		credentials: true,
+	},
+});
+// connection event listener -> handles all incoming connections form clients
+
+app.get("/", (req, res) => {
+	res.redirect(`/${uuidv4()}`);
+});
+
+app.get("/:roomId", (req, res) => {
+	res.render("index", { ROOMID: req.params.roomId });
+});
+
+/* 
+* This signaling server must do.
+* Keeping a list of connected clientsNotifying connected clients when a new client connects
+* Transmitting connection offers from one client to the other
+* Transmitting answers to connection offers
+* Exchanging IceCandidate events between clients
+* Notifying a user when a client disconnects
+
+! Signaling is the process of determining communication protocols, channels, media codecs and formats
+! method of data transfer, and routing information needed to exchange info btn peers.
+
+*/
+
+io.on("connection", (socket) => {
+	socket.on("join-room", (roomid, userid) => {
+		console.log("yo");
+		socket.join(roomid);
+		socket.broadcast
+			.to(roomid)
+			.emit("user-connected", userid, (type = "participant"));
+		socket.on("message", (msg) => {
+			socket.to(roomid).emit("createMessage", msg);
+		});
+	});
+});
+
+httpServer.listen(3000);
